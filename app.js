c26fecac23d7523fa0625fd22b97b097a7dd6c2a
Samm96 <sihorsch96@gmail.com>
2022-05-29 15:52:16 -0400

    Add placeholder cards/users files & setting up appjs get

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const { PORT = 3000 } = process.env;
 
+const users = require('./users');
+const cards = require('./cards');
+
 const app = express();
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
+})
+
+app.get('/users', (req, res) =>{
+  res.send(users);
+})
+
+app.get('/cards', (req, res) => {
+  res.send(cards);
+})
+
+// here down doesn't work properly
+
+app.get('/users/:id', (req, res) => {
+  const { name, about, avatar, _id } = req.params;
+
+  if (!users[_id]) {
+    res.send({"message": "User ID not found"});
+    return;
+  }
+
+  res.send(users[_id]);
 })
