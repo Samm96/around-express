67b70be5e9e1106822fbfdee3ddf30c25a326ed4
Samm96 <sihorsch96@gmail.com>
2022-05-31 19:28:22 -0400

    Add: controllers & routes; Refactor: moved previous code into proper locations

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
 const express = require('express');
 const { PORT = 3000 } = process.env;
 
-const users = require('./users');
-const cards = require('./cards');
-
 const app = express();
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 })
 
-app.get('/users', (req, res) =>{
-  res.send(users);
-})
-
-app.get('/cards', (req, res) => {
-  res.send(cards);
-})
-
-app.get('/users/:id', (req, res) => {
-
-  if (!users[req.params.id]) {
-    res.send({"message": "User ID not found"});
-    return;
-  }
-
-  res.send(users[req.params.id]);
-})
-
-app.get('*', (req, res) => {
-  res.status(404).send({"message": "Requested resource not found"});
-})
