da841c23d04c08b83c21cb4815111d123b9382d9
Samm96 <sihorsch96@gmail.com>
2022-06-12 14:46:54 -0400

    Add: Installed Mongoose via appjs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
