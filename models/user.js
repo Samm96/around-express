20dc822cbb568b324fab4bb63d56791197dc1d14
Samm96 <sihorsch96@gmail.com>
2022-06-13 21:19:59 -0400

    Add: via usermodeljs added error messages & avatar validator

--- models/user.js
+++ models/user.js
@@ -3,22 +3,27 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 2,
-    maxLength: 30,
-    required: true,
+    minLength: [2, 'The "name" field length is too short (min: 2)'],
+    maxLength: [30,'The "name" field length is too long (max: 30)'],
+    required: [true,'Required field'],
   },
 
   about: {
     type: String,
-    minLength: 2,
-    maxLength: 30,
-    required: true,
+    minLength: [2, 'The "about" field length is too short (min: 2)'],
+    maxLength: [30,'The "about" field length is too long (max: 30)'],
+    required: [true,'Required field'],
   },
 
   avatar: {
     type: String,
-    required: true,
-    pattern: /(^http|https)?:\/\/?(^www\.)?\S{1,}/,
+    required: [true,'Required field'],
+    validate: {
+      validator: (v) => {
+        return /(^http|https)?:\/\/?(^www\.)?\S{1,}\.[a-z]{3}?\/\S{1,}/.test(v);
+      },
+      message: 'This is not a valid URL'
+    },
   },
 });
 
