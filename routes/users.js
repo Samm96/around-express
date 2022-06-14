a449f77aa970374a0b704ac6efd3734967bcd354
Samm96 <sihorsch96@gmail.com>
2022-06-14 19:06:27 -0400

    Add: router patch paths usersjs

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
-const { getUser, getUsers, createUser } = require('../controllers/users');
+const { getUser, getUsers, createUser, updateUser, updateAvatar } = require('../controllers/users');
 
 router.get('/', getUsers);
 
 router.get('/:id', getUser);
 
 router.post('/', createUser);
+
+router.patch('/me', updateUser);
+
+router.patch('/avatar', updateAvatar);
 
 module.exports = router;
