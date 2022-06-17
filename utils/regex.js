c32d19a8b8d868b6497025c49b8807d16382240c
Samm96 <sihorsch96@gmail.com>
2022-06-17 17:49:10 -0400

    Delete: regex trials

--- utils/regex.js
+++ utils/regex.js
@@ -1,11 +1,5 @@
 const linkRegex = /(^http|https)?:\/\/(^w{3}\.)?\S{1,}\.([a-z]{3})\/?(\S{1,})?/;
 
-// /(^http|https)?:\/\/?(w{3}\.)?\S{1,}\.([a-z]{3})?(\S{1,})/
-
-// /(^http|https)?:\/\/?(w{3}\.)?\S{1,}\.[a-z]{3}/
-
-// /(^http|https)?:\/\/?(^www\.)?\S{1,}\.[a-z]{3}?\/\S{1,}/;
-
 module.exports = {
   linkRegex
 };
